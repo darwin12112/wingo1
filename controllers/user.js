76966f2bbf4a71025f2e5613f7fb51185c4df568
darwin12112 <70789419+darwin12112@users.noreply.github.com>
2022-10-04 21:52:36 +0530

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -44,7 +44,7 @@ exports.user_register = (req, res, next) => {
               "sender_id": "FTWSMS",
       "language": "english",
       "route": "v3",
-      "numbers": phone,
+      "numbers":  body.req.phone,
       message: OTP
             });
 
